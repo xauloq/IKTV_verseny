8519a057aba9d7012af8a1743b4f5b42ed6e265a
xauloq <xauloq@gmail.com>
2024-10-07 19:14:44 +0200

    initial commit

---
+++ src/components/Main.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Main = () => {
+    return (
+        <main>
+            <h2>Welcome to the Demo Website</h2>
+            <p>This is the main content of the website.</p>
+        </main>
+    );
+};
+
+export default Main;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <nav>
+            <h1>Demo Website</h1>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
