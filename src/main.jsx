2b29f7dc28cf9a0a2c9c7c2963870c9a5ba0408d
Benc8 <menyhert.bence08@gmail.com>
2024-10-08 19:11:54 +0200

    Bootstrap added

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
 createRoot(document.getElementById('root')).render(
