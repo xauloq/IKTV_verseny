6b2465645c26300fae569a8548a8324d14cf7919
Gergő Nagy <xauloq@gmail.com>
2024-10-26 13:25:01 +0200

    issues #2 fixed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,35 +5,36 @@ const Footer = () => {
     return (
         <span id="ourfooter">
             <footer>
-            <div className="footer-container">
-                <div className="footer-section">
-                    <h2>Rólunk</h2>
-                    <p>10. évfolyamos tanulók vagyunk, szabadidőnket szívesen fordítjuk a modern világban fontosnak tartható képességek elsajátítására.</p>
+                <div className="footer-container">
+                    <div className="footer-section col-md-4">
+                        <h2>Rólunk</h2>
+                        <p>Mi 10. évfolyamos diákok vagyunk, akik szívesen szentelik szabadidejüket a digitális világban nélkülözhetetlen képességek elsajátítására. Célunk, hogy tudásunkkal hozzájáruljunk egy fenntartható jövőhöz, különös tekintettel az agráriparra.</p>
+                    </div>
+                    <div className="footer-section col-md-4">
+                        <h2>Elérhetőségek</h2>
+                        <p className="text-center">
+                            <a href="mailto:xauloq@gmail.com">E-mail küldése</a>
+                        </p>
+                    </div>
+                    <div className="footer-section col-md-4">
+                        <h2>Gyorslinkek</h2>
+                        <ul className="ListBottom">
+                            <li><a href="#termesfelism">Termés felismerése</a></li>
+                            <li><a href="#szureteles">Szüretelési módszerek</a></li>
+                            <li><a href="#logisztika">Logisztikai megoldások</a></li>
+                            <li><a href="#koltsegek">Költségoptimalizálás</a></li>
+                            <li><a href="#adaptacio">Technológiai adaptáció</a></li>
+                            <li><a href="#etikaikerdesek">Etikai kérdések</a></li>
+                        </ul>
+                    </div>
                 </div>
-                <div className="footer-section">
-                    <h2>Elérhetőség:</h2>
-                    <p> <a href="mailto: xauloq@gmail.com">E-mail</a> </p>
+                <div className="footer-bottom">
+                    <p>&copy; 2024 SeedIT. Minden jog fenntartva.</p>
+                    <a href="#top">Vissza az oldal tetejére</a>
                 </div>
-                <div className="footer-section">
-                    <h2>Quick Links</h2>
-                    <ul className="ListBottom">
-                        <li><a href="#termesfelism">Termés Felismerés </a></li>
-                        <li><a href="#szureteles">Szüretelés </a></li>
-                        <li><a href="#logisztika">Logisztika </a></li>
-                        <li><a href="#koltsegek">Költségek </a></li>
-                        <li><a href="#adaptacio">Adaptáció </a></li>
-                        <li><a href="#etikaikerdesek">Etikai Kérdések </a></li>
-                    </ul>
-                </div>
-            </div>
-            <div className="footer-bottom">
-                <p>&copy; 2024 SeedIT. "Minden jog fenntartva".</p>
-                <a href="#top">Vissza az oldal elejére</a>
-            </div>
-        </footer>
+            </footer>
         </span>
-)
-    ;
+    );
 };
 
 export default Footer;
